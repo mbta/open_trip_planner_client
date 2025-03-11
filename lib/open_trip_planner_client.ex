1c696c1b5d30be0248e0f704f01f3431f2e0e356
Josh Larson <jlarson@mbta.com>
2025-03-11 11:15:45 -0400

    fixup! feat: Allow multiple GTFS sources

--- lib/open_trip_planner_client.ex
+++ lib/open_trip_planner_client.ex
@@ -23,7 +23,6 @@ defmodule OpenTripPlannerClient do
   Generate a trip plan with the given endpoints and options.
   """
   def plan(from, to, opts) do
-    dbg("DOOOO THE PLAN")
     {tags, opts} = Keyword.pop(opts, :tags, default_tags(opts))
 
     case ParamsBuilder.build_params(from, to, opts) do
@@ -93,7 +92,6 @@ defmodule OpenTripPlannerClient do
     |> Req.new()
     |> AbsintheClient.attach()
     |> Req.post(graphql: {@plan_query, params})
-    |> dbg()
   end
 
   defp plan_url do
