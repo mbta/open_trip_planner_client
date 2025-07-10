d38fd9bf3f0f382cef4fd9d46b2d9bbe512245ed
Cristen Jones <cjones3@mbta.com>
2025-07-10 11:57:50 -0400

    remove unneeded clauses

--- lib/gettext_plural.ex
+++ lib/gettext_plural.ex
@@ -10,25 +10,17 @@ defmodule OpenTripPlannerClient.GettextPlural do
   """
   @behaviour Gettext.Plural
 
-  # Haitian Creole is not supported by default by Gettext.Plural (which uses Expo.PluralForms)
+  # Haitian Creole is not supported by default by Gettext.Plural
   @impl Gettext.Plural
   def nplurals("ht"), do: 2
-
-  # Gettext.Plural forwards xx_YY to xx for plurals, but not xx_YY, so this patches that
-  def nplurals(<<locale::binary-size(2)>> <> "_" <> _sublocale),
-    do: Gettext.Plural.nplurals(locale)
 
   # Fall back to Gettext.Plural
   defdelegate nplurals(locale), to: Gettext.Plural
 
-  # Haitian Creole is not supported by default by Gettext.Plural (which uses Expo.PluralForms)
+  # Haitian Creole is not supported by default by Gettext.Plural
   @impl Gettext.Plural
   def plural("ht", 1), do: 0
   def plural("ht", _), do: 1
-
-  # Gettext.Plural forwards xx_YY to xx for plurals, but not xx_YY, so this patches that
-  def plural(<<locale::binary-size(2)>> <> "_" <> _sublocale, count),
-    do: Gettext.Plural.plural(locale, count)
 
   # Fall back to Gettext.Plural
   defdelegate plural(locale, n), to: Gettext.Plural
