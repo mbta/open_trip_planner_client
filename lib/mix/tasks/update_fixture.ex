5ca0647ddb65cc151bddb1ef88e746a14282a9d7
Cristen Jones <cjones3@mbta.com>
2026-08-13 12:43:17 -0400

    chore: update the test fixture

--- lib/mix/tasks/update_fixture.ex
+++ lib/mix/tasks/update_fixture.ex
@@ -3,7 +3,7 @@ defmodule Mix.Tasks.UpdateFixture do
   @moduledoc "Run: `mix update_fixture` to request new data."
   use Mix.Task
 
-  alias OpenTripPlannerClient.{Parser, PlanParams, Request}
+  alias OpenTripPlannerClient.{PlanParams, Request}
 
   @spec run(command_line_args :: [binary]) :: any()
   def run(_) do
@@ -17,10 +17,7 @@ defmodule Mix.Tasks.UpdateFixture do
       })
 
     {:ok, %{body: body}} = Request.plan_connection(params)
-    {:ok, query_result} = Parser.validate_body(body)
-
-    encoded = Jason.encode!(%{"data" => Nestru.encode!(query_result)}, pretty: true)
-
+    encoded = Jason.encode!(body, pretty: true)
     File.write("test/fixture/alewife_to_franklin_park_zoo.json", encoded)
   end
 end
